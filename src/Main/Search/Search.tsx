afd16b2d38e1a495d4f715899ac603c58528d744
Ioannis <giannhs95@yahoo.gr>
2022-11-17 11:11:06 +0100

    Apply focus on search input by clicking anywhere on the search div

--- src/Main/Search/Search.tsx
+++ src/Main/Search/Search.tsx
@@ -1,10 +1,25 @@
 import "./Search.scss"
 import searchIcon from "../../assets/icons/search.png"
+import { useRef } from 'react';
+
 const Search = () => {
+	const inputRef = useRef<HTMLInputElement>(null);
+
+	const handleClick = () => {
+		if (inputRef.current) {
+			inputRef.current.focus();
+		}
+	}
+
 	return (
-		<div className="Search">
+		<div className="Search" onClick={handleClick}>
 			<img src={searchIcon} />
-			<input type="text" placeholder="Search Twitter" className="Search" />
+			<input 
+				ref={inputRef}
+				type="text" 
+				placeholder="Search Twitter" 
+				className="Search" 
+			/>
 		</div>
 	)
 }
