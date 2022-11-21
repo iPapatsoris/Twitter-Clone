474587a4eabea8251a68b1af4c19a407f09dd0c6
Ioannis <giannhs95@yahoo.gr>
2022-11-21 16:32:49 +0100

    Make StickyInbox cover more area

--- src/Main/Inbox/StickyInbox/StickyInbox.tsx
+++ src/Main/Inbox/StickyInbox/StickyInbox.tsx
@@ -6,10 +6,12 @@ import expandInbox from "../../../assets/icons/expand-inbox.png"
 const InboxSticky = () => {
 	return (
 		<div className="InboxSticky">
-			<h3 className="BiggerText">Messages</h3>
-			<div className="RightAligned">
-				<img src={newMessageIcon} />
-				<img src={expandInbox} />
+			<div className="InboxStickyContent">
+				<h3 className="BiggerText">Messages</h3>
+				<div className="RightAligned">
+					<img src={newMessageIcon} />
+					<img src={expandInbox} />
+				</div>
 			</div>
 		</div>
 	)
