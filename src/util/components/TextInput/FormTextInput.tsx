4b1efd0500efb2a80a399a7f6ed57a949f6d613f
Ioannis <giannhs95@yahoo.gr>
2023-03-15 17:33:19 +0100

    Tighten conditions for reporting FormTextInput errors

--- src/util/components/TextInput/FormTextInput.tsx
+++ src/util/components/TextInput/FormTextInput.tsx
@@ -1,6 +1,11 @@
-import { Controller, FieldValues, Path, UseFormReturn } from "react-hook-form";
+import {
+  FieldValues,
+  Path,
+  useController,
+  UseFormReturn,
+} from "react-hook-form";
 import Input from "./TextInput";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TextInput from "./TextInput";
 
 type FormInputProps<FormInput extends FieldValues> = Omit<
@@ -11,6 +16,12 @@ type FormInputProps<FormInput extends FieldValues> = Omit<
   name: Path<FormInput>;
 } & Pick<React.ComponentProps<typeof TextInput>, "type">;
 
+/**
+ * react-hook-form wrapper for custom controller Input component.
+ * Extra logic to expand the library's "onBlur" mode prop for useForm:
+ * the errors that would be reported on input blur, are reported only if the
+ * input has been dirtied at least once before.
+ */
 const FormInput = <FormInput extends FieldValues>({
   name,
   control,
@@ -18,31 +29,41 @@ const FormInput = <FormInput extends FieldValues>({
   maxLength,
   autofocus,
   type,
-}: FormInputProps<FormInput>) => (
-  <Controller
-    name={name}
-    control={control}
-    render={({
-      field: { onChange, onBlur, value, name, ref },
-      fieldState: { invalid, isTouched, isDirty, error },
-      formState,
-    }) => {
-      return (
-        <Input
-          name={name}
-          placeholder={placeholder}
-          maxLength={maxLength !== undefined ? maxLength : undefined}
-          autofocus={autofocus !== undefined ? autofocus : undefined}
-          onBlur={onBlur}
-          onChange={onChange}
-          value={value}
-          ref={ref}
-          error={error?.message}
-          type={type}
-        />
-      );
-    }}
-  ></Controller>
-);
+}: FormInputProps<FormInput>) => {
+  const {
+    field: { onChange, onBlur, value, ref },
+    fieldState: { isDirty, error },
+  } = useController({ name, control });
+  const [hasBeenDirtied, setHasBeenDirtied] = useState(false);
+  const [showErrors, setShowErrors] = useState(false);
+
+  useEffect(() => {
+    if (isDirty && !hasBeenDirtied) {
+      setHasBeenDirtied(true);
+    }
+  }, [isDirty, hasBeenDirtied, setHasBeenDirtied]);
+
+  const handleBlur = () => {
+    onBlur();
+    if (hasBeenDirtied) {
+      setShowErrors(true);
+    }
+  };
+
+  return (
+    <Input
+      name={name}
+      placeholder={placeholder}
+      maxLength={maxLength !== undefined ? maxLength : undefined}
+      autofocus={autofocus !== undefined ? autofocus : undefined}
+      onBlur={handleBlur}
+      onChange={onChange}
+      value={value}
+      ref={ref}
+      error={showErrors && error ? error.message : ""}
+      type={type}
+    />
+  );
+};
 
 export default FormInput;
