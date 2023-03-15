51b2e1a1fa45c9d8443a308bec3ed8ad934aa438
Ioannis <giannhs95@yahoo.gr>
2023-03-15 17:18:16 +0100

    Setup react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./global.css";
 import Router from "./Router";
 import { AuthContext } from "./util/hooks/useAuth";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AuthContext.Provider
-      value={
-        {
-          // user: { id: 1, avatar: "adsf", username: "adsf", name: "asdf" },
+    <QueryClientProvider client={queryClient}>
+      <AuthContext.Provider
+        value={
+          {
+            // user: { id: 1, avatar: "adsf", username: "adsf", name: "asdf" },
+          }
         }
-      }
-    >
-      <Router />
-    </AuthContext.Provider>
+      >
+        <Router />
+      </AuthContext.Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/util/api.ts
@@ -0,0 +1,16 @@
+const getPath = (path: string) => {
+  const base = import.meta.env.VITE_API_PATH;
+  return base + path;
+};
+
+export const postData = async (path: string, body: any) => {
+  const res = await fetch(getPath(path), {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return res.json();
+};
