66d0e5aa89b713d23bfa3661411588d5db0891b0
Ioannis <giannhs95@yahoo.gr>
2023-09-21 16:34:31 +0200

    Add sidebar vertial scrollbar and padding when it overflows

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -5,9 +5,29 @@ import ProfileButton from "./ProfileButton/ProfileButton";
 import Button from "../util/components/Button/Button";
 import Icon from "../util/components/Icon/Icon";
 import useWindowDimensions from "../util/hooks/useWindowDimensions";
+import { useEffect, useRef } from "react";
 
 const Sidebar = () => {
-  const { isPcBig, isSmallScreen } = useWindowDimensions();
+  const { isPcBig, isSmallScreen, height } = useWindowDimensions();
+  const ref = useRef<HTMLElement>(null);
+
+  /* Add vertical scrollbar when sidebar overflows and padding.
+     Padding is needed because we have icon hover effects that overflow 
+     horizontally, but specifying "overflow-y: auto" cuts short the hover effect 
+     on the left. We check manually for overflow because we don't want to be 
+     forced to always use that padding.
+  */
+  useEffect(() => {
+    if (ref && ref.current) {
+      if (isPcBig && height < ref.current.scrollHeight) {
+        ref.current.style.overflowY = "auto";
+        ref.current.style.paddingLeft = "1rem";
+      } else {
+        ref.current.style.overflowY = "visible";
+        ref.current.style.paddingLeft = "0";
+      }
+    }
+  }, [height, ref, isPcBig]);
 
   const postTweetButton = isPcBig ? (
     <div className={styles.TweetButtonWrapper}>
@@ -29,7 +49,7 @@ const Sidebar = () => {
   );
 
   return (
-    <header className={styles.Sidebar}>
+    <header className={styles.Sidebar} ref={ref}>
       <Nav />
       {!isSmallScreen && postTweetButton}
       {!isSmallScreen && <ProfileButton />}
