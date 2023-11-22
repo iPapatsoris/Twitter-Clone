77b7ef0e1ab5fa8d72a24a2b4dfecc069c2661e9
Ioannis <giannhs95@yahoo.gr>
2023-11-22 21:11:47 +0100

    Fix layout shift on mobile sidepanel click

--- src/Main/layouts/Header/HeaderMain/HeaderAvatar.tsx
+++ src/Main/layouts/Header/HeaderMain/HeaderAvatar.tsx
@@ -3,10 +3,26 @@ import Modal from "../../../../util/components/Modal/Modal";
 import MobileSidepanel from "../../../../Sidebar/MobileSidepanel";
 import Avatar from "../../../routes/Profile/ProfileFace/Avatar/Avatar";
 import { useLoggedInUser } from "../../../../store/AuthStore";
+import { useQueryClient } from "@tanstack/react-query";
+import {
+  mediumPreviewProfileFields,
+  profileKeys,
+} from "../../../routes/Profile/ProfileFace/queries";
 
 const HeaderAvatar = () => {
   const [showMobileSidePanel, setShowMobileSidePanel] = useState(false);
   const loggedInUser = useLoggedInUser();
+
+  const queryClient = useQueryClient();
+
+  const onAvatarClick = async () => {
+    await queryClient.ensureQueryData(
+      profileKeys
+        .username(loggedInUser!.username)
+        ._ctx.fields(mediumPreviewProfileFields)
+    );
+    setShowMobileSidePanel(true);
+  };
   return (
     <>
       {showMobileSidePanel && (
@@ -14,11 +30,7 @@ const HeaderAvatar = () => {
           <MobileSidepanel />
         </Modal>
       )}
-      <Avatar
-        size="tiny"
-        src={loggedInUser?.avatar}
-        onClick={() => setShowMobileSidePanel(true)}
-      />
+      <Avatar size="tiny" src={loggedInUser?.avatar} onClick={onAvatarClick} />
     </>
   );
 };
